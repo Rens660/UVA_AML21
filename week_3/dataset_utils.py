39d147d5897d77529a881ebbb11a76b008ed65fe
Rens660 <rensjh@gmail.com>
2021-11-19 22:56:49 +0100

    one

--- week_3/dataset_utils.py
+++ week_3/dataset_utils.py
@@ -15,10 +15,10 @@ def load_mnist(dataset = "training", path = "."):
     being a numpy.uint8 2D array of pixel data for the given image.
     """
 
-    if dataset is "training":
+    if dataset == "training":
         fname_img = os.path.join(path, 'train-images-idx3-ubyte')
         fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
-    elif dataset is "testing":
+    elif dataset == "testing":
         fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
         fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
     else:
@@ -28,7 +28,7 @@ def load_mnist(dataset = "training", path = "."):
     with open(fname_lbl, 'rb') as flbl:
         magic, num = struct.unpack(">II", flbl.read(8))
         lbl = np.fromfile(flbl, dtype=np.int8)
-
+    print(lbl[0])
     with open(fname_img, 'rb') as fimg:
         magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
         img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), cols, rows)
